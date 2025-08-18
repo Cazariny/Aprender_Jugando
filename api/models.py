7301f02746000ae999e001fa02e9b6baafbb0267
jared <jared.guz.morales@gmail.com>
2025-08-18 12:36:23 -0600

    perfil completo, TYC pagina

--- api/models.py
+++ api/models.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 class UsuarioPersonalizado(AbstractUser):
     OPCIONES_MEMBRESIA = [
         ('regular', 'Usuario regular'),
